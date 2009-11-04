3413ac708dd5e9a7105bf7df7ac43873cf6499a3
Ryan Li <ryan@ryanium.com>
2009-11-04 15:09:57 +0800

    Added two maintainer utilities.

---
+++ makexpi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# makexpi.sh
+# This script is used for generate xpi file
+# from the latest source.
+#
+# Only tested under GNU/Linux, for maintainer
+# use only.
+#
+# Licensed under GNU GPLv3.
+# Copyright (c) 2009, Ryan Li <ryan@ryanium.com>
+
+zip -q character-palette.xpi -r * -x makexpi.sh versionbump.sh
---
+++ versionbump.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# versiondump.sh
+# Script used for updating version strings
+# in source files.
+#
+# Usage:
+# ./versiondump.sh {VERSION}
+# Example:
+# ./versiondump.sh 0.1
+#
+# Copyright (c) 2009 Ryan Li
+
+sed -i "s|\"software-version\">.*<|\"software-version\">$1<|g" ./chrome/content/about.xul
+sed -i "s|<em:version>.*</em:version>|<em:version>$1</em:version>|g" ./install.rdf
