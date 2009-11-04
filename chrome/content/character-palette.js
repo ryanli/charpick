2a5a13f90f0ceb89bec0972af704125aac67c6dd
Ryan Li <ryan@ryanium.com>
2009-11-04 19:49:49 +0800

    Moved palettes from XUL to JS, for future extension.

--- chrome/content/character-palette.js
+++ chrome/content/character-palette.js
@@ -14,3 +14,23 @@ characterpalette_copyToClipboard = function(str)
 	getService(Components.interfaces.nsIClipboardHelper);
 	gClipboardHelper.copyString(str);
 };
+
+characterpalette_addPalettes = function()
+{
+	var palettes = ["ÁÀĂÂǍÄÃȀÅĀ", "áàăâǎäãȁåā", "ĆćĈĉČčÇç¢ñ", "ÉéÈèĔĕÊêĚě", "ÍÌĬÎǏÏĮĨĪȈ", "íìĭîǐïįĩīȉ",
+		"ǑȌÝýỲỳŶŷ¥£", "óòŏôǒöőõøȍ", "ÚÙŬÛǓŮÜŰŨŪ", "úùŭûǔůüűũū"];
+	var popup = document.getElementById("characterpalette-popup");
+	var seperator = document.getElementById("characterpalette-menu-seperator");
+	for (var index in palettes)
+	{
+		var palette = document.createElement("menuitem");
+		palette.setAttribute("class", "charset");
+		palette.setAttribute("group", "charset");
+		palette.setAttribute("type", "radio");
+		palette.setAttribute("oncommand", "characterpalette_loadPalette(this.label);");
+		palette.setAttribute("label", palettes[index]);
+		popup.insertBefore(palette, seperator);
+	}
+};
+
+window.addEventListener("load", characterpalette_addPalettes, false);
