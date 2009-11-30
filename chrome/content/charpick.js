34a0583b6a33cfc9eaf3a79d8c0dac824a7d0b8d
Ryan Li <ryan@ryanium.com>
2009-11-30 17:15:51 +0800

    Change character-palette to charpick in codes.

--- chrome/content/charpick.js
+++ chrome/content/charpick.js
@@ -2,10 +2,10 @@ if (!com)
 	var com = {};
 if (!com.ryanium)
 	com.ryanium = {};
-if (!com.ryanium.characterpalette)
-	com.ryanium.characterpalette = {};
+if (!com.ryanium.charpick)
+	com.ryanium.charpick = {};
 
-com.ryanium.characterpalette = {
+com.ryanium.charpick = {
 
 // utils begin
 	prefManager : Components
@@ -15,7 +15,7 @@ com.ryanium.characterpalette = {
 	getStringPref : function(key)
 	{
 		return this.prefManager.
-			getComplexValue("extensions.character-palette." + key, Components.interfaces.nsISupportsString)
+			getComplexValue("extensions.charpick." + key, Components.interfaces.nsISupportsString)
 			.data;
 	},
 
@@ -26,17 +26,17 @@ com.ryanium.characterpalette = {
 			.createInstance(Components.interfaces.nsISupportsString);
 		str.data = val;
 		this.prefManager
-			.setComplexValue("extensions.character-palette." + key, Components.interfaces.nsISupportsString, str);
+			.setComplexValue("extensions.charpick." + key, Components.interfaces.nsISupportsString, str);
 	},
 
 	getIntegerPref : function(key)
 	{
-		return this.prefManager.getIntPref("extensions.character-palette." + key);
+		return this.prefManager.getIntPref("extensions.charpick." + key);
 	},
 
 	setIntegerPref : function(key, val)
 	{
-		this.prefManager.setIntPref("extensions.character-palette." + key, val);
+		this.prefManager.setIntPref("extensions.charpick." + key, val);
 	},
 
 	setClipboard : function(str)
@@ -51,7 +51,7 @@ com.ryanium.characterpalette = {
 // palette prefs begin
 	getPalettes : function()
 	{
-		var palettes = com.ryanium.characterpalette.getStringPref("palettes");
+		var palettes = com.ryanium.charpick.getStringPref("palettes");
 		var index = 0;
 		var ret = new Array();
 		while (index < palettes.length)
@@ -86,16 +86,16 @@ com.ryanium.characterpalette = {
 		var merged = new String();
 		for (var index in a)
 			merged += a[index].replace(/;/g, ';;') + ';';
-		com.ryanium.characterpalette.setStringPref('palettes', merged);
+		com.ryanium.charpick.setStringPref('palettes', merged);
 	},
 // palette prefs end
 
 // toolbar actions begin
 	loadPalettes : function()
 	{
-		var palettes = com.ryanium.characterpalette.getPalettes();
-		var popup = document.getElementById("character-palette-popup");
-		var seperator = document.getElementById("character-palette-menu-seperator");
+		var palettes = com.ryanium.charpick.getPalettes();
+		var popup = document.getElementById("charpick-popup");
+		var seperator = document.getElementById("charpick-menu-seperator");
 
 		while (popup.getElementsByClassName("charset").length)
 			popup.removeChild(popup.getElementsByClassName("charset")[0]);
@@ -106,35 +106,35 @@ com.ryanium.characterpalette = {
 			palette.setAttribute("class", "charset");
 			palette.setAttribute("group", "charset");
 			palette.setAttribute("type", "radio");
-			palette.setAttribute("oncommand", "com.ryanium.characterpalette.selectPalette(" + index + ", this.label);");
+			palette.setAttribute("oncommand", "com.ryanium.charpick.selectPalette(" + index + ", this.label);");
 			palette.setAttribute("label", palettes[index]);
 			popup.insertBefore(palette, seperator);
 		}
 		var selected = 0;
-		if (com.ryanium.characterpalette.getIntegerPref("selected"))
-			selected = com.ryanium.characterpalette.getIntegerPref("selected");
+		if (com.ryanium.charpick.getIntegerPref("selected"))
+			selected = com.ryanium.charpick.getIntegerPref("selected");
 		if (!popup.getElementsByClassName("charset")[selected])
 			selected = 0;
 		popup.getElementsByClassName("charset")[selected].setAttribute("checked", "true");
-		com.ryanium.characterpalette.selectPalette(selected, palettes[selected]);
+		com.ryanium.charpick.selectPalette(selected, palettes[selected]);
 	},
 
 	selectPalette : function(index, charset)
 	{
 		this.selected = '';
-		com.ryanium.characterpalette.setIntegerPref("selected", index);
-		var container = document.getElementById("character-palette-buttons");
+		com.ryanium.charpick.setIntegerPref("selected", index);
+		var container = document.getElementById("charpick-buttons");
 		while (container.childNodes.length)
 			container.removeChild(container.firstChild);
 
 		for (var charIndex in charset)
 		{
 			var charButton = document.createElement("toolbarbutton");
-			charButton.setAttribute("id", "character-palette-char-" + charIndex);
-			charButton.setAttribute("class", "character-palette-char");
-			charButton.setAttribute("group", "character-palette-char");
+			charButton.setAttribute("id", "charpick-char-" + charIndex);
+			charButton.setAttribute("class", "charpick-char");
+			charButton.setAttribute("group", "charpick-char");
 			charButton.setAttribute("type", "radio");
-			charButton.setAttribute("oncommand", "com.ryanium.characterpalette.selectChar(this);");
+			charButton.setAttribute("oncommand", "com.ryanium.charpick.selectChar(this);");
 			charButton.setAttribute("label", charset[charIndex]);
 			container.appendChild(charButton);
 		}
@@ -150,7 +150,7 @@ com.ryanium.characterpalette = {
 		else
 		{
 			this.selected = obj.id;
-			com.ryanium.characterpalette.setClipboard(obj.getAttribute('label'));
+			com.ryanium.charpick.setClipboard(obj.getAttribute('label'));
 		}
 	},
 // toolbar actions end
@@ -158,37 +158,37 @@ com.ryanium.characterpalette = {
 // preferences dialog begin
 	loadPaletteList : function()
 	{
-		var list = document.getElementById("character-palette-palette-list");
-		var palettes = com.ryanium.characterpalette.getPalettes();
+		var list = document.getElementById("charpick-palette-list");
+		var palettes = com.ryanium.charpick.getPalettes();
 		for (index in palettes)
 			list.appendItem(palettes[index]);
 	},
 
 	savePalettes : function()
 	{
-		var list = document.getElementById("character-palette-palette-list");
+		var list = document.getElementById("charpick-palette-list");
 		if (list == null)
 			return true;
 		var palettes = new Array();
 		for (var child = list.firstChild; child != null; child = child.nextSibling)
 			palettes.push(child.getAttribute('label'));
-		com.ryanium.characterpalette.setPalettes(palettes);
+		com.ryanium.charpick.setPalettes(palettes);
 		return true;
 	},
 
 	addPalette : function()
 	{
-		var list = document.getElementById("character-palette-palette-list");
+		var list = document.getElementById("charpick-palette-list");
 		var params = {add: false, palette: ""};
-		window.openDialog('chrome://character-palette/content/add-palette.xul',
-			'character-palette-add-palette-dialog', 'centerscreen,chrome,modal', params);
+		window.openDialog('chrome://charpick/content/add-palette.xul',
+			'charpick-add-palette-dialog', 'centerscreen,chrome,modal', params);
 		if (params.add)
 			list.appendItem(params.palette);
 	},
 
 	editPalette : function()
 	{
-		var list = document.getElementById("character-palette-palette-list");
+		var list = document.getElementById("charpick-palette-list");
 		if (list)
 		{
 			var selected = list.selectedItem;
@@ -196,8 +196,8 @@ com.ryanium.characterpalette = {
 			{
 				var orig = selected.label;
 				var params = {edit: false, palette: orig};
-				window.openDialog('chrome://character-palette/content/edit-palette.xul',
-					'character-palette-edit-palette-dialog', 'centerscreen,chrome,modal', params);
+				window.openDialog('chrome://charpick/content/edit-palette.xul',
+					'charpick-edit-palette-dialog', 'centerscreen,chrome,modal', params);
 				if (params.edit)
 					selected.label = params.palette;
 			}
@@ -206,14 +206,14 @@ com.ryanium.characterpalette = {
 
 	deletePalette : function()
 	{
-		var list = document.getElementById("character-palette-palette-list");
+		var list = document.getElementById("charpick-palette-list");
 		if (list)
 			list.removeItemAt(list.selectedIndex);
 	},
 
 	setTextBox : function()
 	{
-		var textbox = document.getElementById("character-palette-textbox");
+		var textbox = document.getElementById("charpick-textbox");
 		textbox.value = window.arguments[0].palette;
 	},
 // preferences dialog end
@@ -221,9 +221,9 @@ com.ryanium.characterpalette = {
 // listener begin
 	init : function()
 	{
-		com.ryanium.characterpalette.loadPalettes();
+		com.ryanium.charpick.loadPalettes();
 	}
 // listener end
 };
 
-window.addEventListener("load", com.ryanium.characterpalette.init, false);
+window.addEventListener("load", com.ryanium.charpick.init, false);
