0d89d325b6f8ba010f45d1d13807379d239d19f2
Ryan Li <ryan@ryanium.com>
2009-12-03 10:20:37 +0800

    Changed ID in prefs.js

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,3 +1,3 @@
-pref("extensions.{5c17eb1d-edfa-5f58-3a64-a49f2071713c}.description", "chrome://charpick/locale/charpick.properties");
+pref("extensions.charpick@ryanium.com.description", "chrome://charpick/locale/charpick.properties");
 pref("extensions.charpick.palettes", "áàéíñó½©ث€;áàâãäåæª;ÀÁÂÃÄÅÆª;çÇ¢©;èéêëæ;ÈÉÊËÆ;ìíîï;ÌÍÎÏ;ñÑ;òóôõöø°;ÒÓÔÕÖØ°;ß§$;ðÐþÞ;ùúûü;ÙÚÛÜ;ýÝÿ¥;¬­¯±;«»¨·×;¤£¥¢$€;¹²³¼½¾;µ¶®¿¡|;ḓḽṋṱḒḼṊṰṅṄšŠ");
 pref("extensions.charpick.selected", 0);
