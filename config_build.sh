7a82ddd0e19b111838154e2c2a821fa3a2faa2ff
Ryan Li <ryan@ryanium.com>
2010-02-23 10:35:21 +0800

    *: replaced makexpi.sh with build.sh.

---
+++ config_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Build config for the build script, build.sh. Look there for more info.
+
+APP_NAME=charpick
+CHROME_PROVIDERS="chrome"
+CLEAN_UP=1
+ROOT_FILES="README AUTHORS license-gplv3.txt"
+ROOT_DIRS="defaults"
+BEFORE_BUILD=
+AFTER_BUILD=
